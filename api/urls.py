d15836823de6acef2881decc239ef5f78b46495f
Robert <yopuma.silva@gmail.com>
2025-09-03 08:20:20 -0600

    se borran archivos desactualizados

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import hola
-from .views import LoginView
-
-urlpatterns = [
-    path('hola/', hola),
-    path("login/", LoginView.as_view(), name="login"),
-    
-]
